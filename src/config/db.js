6aa41d2bd743cb38b9a12b4d25c280c7db55d261
13krsnaa <saxenakrishna9140@gmail.com>
2026-02-19 17:58:50 +0530

    Database connected

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 function connectToDB() {
+  console.log("DB function called");
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, { serverSelectionTimeoutMS: 5000 })
     .then(() => {
       console.log("server is connected to the DataBase");
     })
     .catch((err) => {
       console.log("Error, Connecting to the DB");
+      console.log(err);
       process.exit(1);
     });
 }
