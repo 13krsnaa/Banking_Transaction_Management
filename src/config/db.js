e903421f5e2f82a4e761f7c62461bf2782750a90
13krsnaa <saxenakrishna9140@gmail.com>
2026-02-20 13:17:27 +0530

    DB.js and added middlewares

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+
+
 
 function connectToDB() {
-  console.log("DB function called");
-  mongoose
-    .connect(process.env.MONGO_URI, { serverSelectionTimeoutMS: 5000 })
-    .then(() => {
-      console.log("server is connected to the DataBase");
-    })
-    .catch((err) => {
-      console.log("Error, Connecting to the DB");
-      console.log(err);
-      process.exit(1);
-    });
+
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("server is connected to DB")
+        })
+        .catch(err => {
+            console.log("Error connecting to DB")
+            process.exit(1)
+        })
+
 }
 
-module.exports = connectToDB;
+
+module.exports = connectToDB
