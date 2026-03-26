af6d63da719171bf315b4892b742a8040bfb8ed6
13krsnaa <saxenakrishna9140@gmail.com>
2026-03-26 09:22:30 +0530

    Basic server setup + database connection done

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
+
+
+
 const app = require("./src/app");
-const connectToDB = require("./src/config/db");
-connectToDB();
+const connectToDb = require("./src/config/db")
+
+
+
+
+
+connectToDb()
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose")
 
-
-
-function connectToDB() {
-
+function connectToDb () {
     mongoose.connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log("server is connected to DB")
-        })
-        .catch(err => {
-            console.log("Error connecting to DB")
-            process.exit(1)
-        })
-
+    .then (()=> {
+        console.log("Connected to Database")
+    })
+    .catch((err) => {
+        console.log(err)
+        process.exit(1) // agar server db se connect nhi hua to server yhi band ho jayega
+    });
 }
 
 
-module.exports = connectToDB
+
+module.exports = connectToDb
