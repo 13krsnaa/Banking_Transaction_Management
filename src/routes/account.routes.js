4c83a0ca0f022711b6524df58255bb4ae06204de
13krsnaa <saxenakrishna9140@gmail.com>
2026-04-16 20:21:54 +0530

    create  working route for creating an account for user

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,5 +1,18 @@
-const express = requuire("express");
+const express = require("express");
+const authMiddleware = require("../middleware/auth.middleware");
+const accountController = require("../controllers/account.conntrollers");
 
 const router = express.Router();
+
+/**
+ * - POST /api/accounts/
+ * - Create a new account for user
+ * - Protected route
+ */
+router.post(
+  "/",
+  authMiddleware.authMiddleware,
+  accountController.createAccountController,
+);
 
 module.exports = router;
