b0f06ff3fda4d1c52d637a969148b47c51e2679a
13krsnaa <saxenakrishna9140@gmail.com>
2026-06-03 11:06:41 +0530

    created authSystemUserMiddleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -21,7 +21,30 @@ async function authMiddleware(req, res, next) {
     res.status(401).json({ message: "Invalid token." });
   }
 }
-
+async function authSystemUserMiddleware(req, res, next) {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1]; //basic check ki token hai ya nahi
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); //token ko verify krna
+    const user = await userModel.findById(decoded.userId).select("+systemUser"); //user ko find krna aur systemUser field ko include krna
+    if (!user || !user.systemUser) {
+      return res
+        .status(403)
+        .json({
+          message: "Access denied. Only system users can perform this action.",
+        });
+    }
+    req.user = user;
+    return next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token." });
+  }
+}
 module.exports = {
   authMiddleware,
+  authSystemUserMiddleware,
 };
