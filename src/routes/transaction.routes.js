f83211f53c33af0220d0a572a89228d359669eda
13krsnaa <saxenakrishna9140@gmail.com>
2026-06-03 11:05:35 +0530

    added transaction route

---
+++ src/routes/transaction.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const transactionRoutes = Router();
+const transactionController = require("../controllers/transaction.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+//POST request: /api/transactions/ ye nya transction create krega
+
+transactionRoutes.post(
+  "/",
+  authMiddleware.authMiddleware,
+  transactionController.createTransaction,
+);
+
+transactionRoutes.post(
+  "/system/initial-funds",
+  authMiddleware.authSystemUserMiddleware,
+  transactionController.createInitialFundsTransaction,
+);
+
+module.exports = transactionRoutes;
