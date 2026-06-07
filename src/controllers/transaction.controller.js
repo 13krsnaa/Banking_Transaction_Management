4ec920f80addc910e19d45c9a71b93cf89c9204b
13krsnaa <saxenakrishna9140@gmail.com>
2026-06-07 17:11:49 +0530

    created 9 steps in transaction.controllers

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -2,6 +2,7 @@ const transactionModel = require("../models/transaction.model");
 const accountModel = require("../models/account.model");
 const ledgerModel = require("../models/ledger.model");
 const emailService = require("../services/email.services");
+const mongoose = require("mongoose");
 
 //* Ye controller me ham apne app ke sare business logic ko implement krte hai *//
 // Creating a new transaction in 10 steps
@@ -19,6 +20,10 @@ const emailService = require("../services/email.services");
     */
 
 async function createTransaction(req, res) {
+  //Step 1 in this function => 1. validate the request  ---req.body se aane wala data ko verify karna
+  /**
+   * Step 1 : Vaildate the request
+   */
   const { freomAccount, toAccount, amount, idempotencyKey } = req.body;
 
   if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
@@ -40,6 +45,108 @@ async function createTransaction(req, res) {
       message: "Invalid fromAccount or toAccount",
     });
   }
+
+  /**
+   * step 2: validate idempotency key
+   */
+
+  const isTransactionalreadyExists = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey,
+  });
+  if (isTransactionalreadyExists) {
+    if (isTransactionalreadyExists.status === "COMPLETED") {
+      return res.status(200).json({
+        message: "Transaction already processed",
+        transaction: isTransactionalreadyExists,
+      });
+    }
+    if (isTransactionalreadyExists.status === "PENDING") {
+      return res.status(200).json({
+        message: "Transaction is already in process",
+        transaction: isTransactionalreadyExists,
+      });
+    }
+    if (isTransactionalreadyExists.status === "FAILED") {
+      return res.status(200).json({
+        message: "Transaction attempt failed. Please try again.",
+        transaction: isTransactionalreadyExists,
+      });
+    }
+    if (isTransactionalreadyExists.status === "REVERSED") {
+      return res.status(200).json({
+        message: "Transaction has been reversed.Please retry.",
+        transaction: isTransactionalreadyExists,
+      });
+    }
+  }
+
+  /**
+   * step 3: check account status
+   */
+  if (
+    fromUserAccount.status !== "ACTIVE" ||
+    toUserAccount.status !== "ACTIVE"
+  ) {
+    return res.send(400).json({
+      message:
+        "Both sender and receiver accounts must be active to process the transaction",
+    });
+  }
+
+  /**
+   * step 4: Derive sender balance from the ledger
+   */
+
+  const balance = await fromUserAccount.getBalance();
+
+  if (balance < amount) {
+    return res.status(400).json({
+      message: `insufficient funds. Current balance is ${balance}`,
+    });
+  }
+
+  /**
+   *  Step 5: Create transaction  with pending status
+   */
+  const session = await mongoose.stratSession();
+  session.startTransaction();
+
+  const transaction = await transaction.create(
+    {
+      fromAccount,
+      toAccount,
+      amount,
+      idempotencyKey,
+      status: "PENDING",
+    },
+    { session },
+  );
+
+  const debitLedgerEntry = await ledgerModel.create(
+    {
+      account: fromAccount,
+      amount: amount,
+      transaction: transaction._id,
+      type: "DEBIT",
+    },
+    { session },
+  );
+
+  const creditLedgerEntry = await ledgerModel.create(
+    {
+      account: toAccount,
+      amount: amount,
+      transaction: transaction._id,
+      type: "CREDIT",
+    },
+    { session },
+  );
+
+  transaction.status = "COMPLETED";
+  await transaction.save({ session });
+
+  await session.commitTransaction();
+  session.endsession();
 }
 
 async function createInitialFundsTransaction(req, res) {
